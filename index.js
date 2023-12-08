3f92706b14c2e5b90c396e0c9a2f25428bd8fa4f
Ernie Crews <141796937+RoyalEC@users.noreply.github.com>
2023-12-08 13:31:52 -0500

    Object Oriented Adventure

---
+++ index.js
@@ -0,0 +1,13 @@
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  companion: {
+    name: "Leo",
+    type: "Cat",
+  },
+};
+
+adventurer.forEach((inventory) => {
+  console.log(inventory[i]);
+});
